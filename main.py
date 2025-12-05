28b43da33c453cab7f2b8a74c0d2d229fed7eea3
otoole-tyler <otooletyler20@gmail.com>
2025-12-05 14:13:36 +1000

    Added proper player movement & drawing

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ clock = pygame.time.Clock()
 Players = []
 
 class Player:
-    def __init__(self, color, keybinds):
+    def __init__(self, color, keybinds, speed):
         self.color = color
         self.keybinds = keybinds # 1:up 2:left 3:down 4:right
 
         self.x = 0
         self.y = 0
 
-        self.direction_changes = []  # stored as [x, y]
+        self.direction_changes = [(self.x, self.y), (self.x, self.y)]  # stored as [x, y]
         self.current_direction = 'up'
+        
+        self.speed = speed
 
     def movement(self):
         keys = pygame.key.get_pressed()
@@ -40,12 +42,20 @@ class Player:
                 changed_direction = True
                 self.current_direction = 'right'
 
+        if changed_direction: self.direction_changes.append((self.x, self.y))#; # print(self.current_direction)
 
-        if changed_direction: self.direction_changes.append([self.x, self.y]); print(self.current_direction)
+        if self.current_direction == 'up': self.y -= self.speed
+        elif self.current_direction == 'left': self.x -= self.speed
+        elif self.current_direction == 'down': self.y += self.speed
+        elif self.current_direction == 'right': self.x += self.speed
+
+    def draw(self):
+        pygame.draw.lines(screen, self.color, False, self.direction_changes, width=2)
+        pygame.draw.line(screen, self.color, self.direction_changes[-1], (self.x, self.y))
             
             
 
-Players.append(Player((255,255,255), [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]))
+Players.append(Player((255,255,255), [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d], 4))
 
 running = True
 while running:
@@ -56,6 +66,7 @@ while running:
 
     for player in Players:
         player.movement()
+        player.draw()
 
     pygame.display.flip()
     clock.tick(60)
