d70b0fe139d88dcdec3ee3d0aeac776ee90530bc
otoole-tyler <otooletyler20@gmail.com>
2025-12-05 13:09:32 +1000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Caption')
+clock = pygame.time.Clock()
+
+class Player:
+    def __init__(self, color, keybinds):
+        self.color = color
+        self.keybinds = keybinds
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+
+
+
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
